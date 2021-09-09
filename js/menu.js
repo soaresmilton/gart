a6163e1572ae1632e4d1b71a355ce67fc198bbd2
soaresmilton <eng.milton.soares@gmail.com>
2021-09-09 14:15:32 -0300

    menu links fix

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,18 @@ const menuSection = document.querySelector('.menu-section');
 const menuToggle = document.querySelector('.menu-toggle');
 
 menuToggle.addEventListener('click', () => { 
-  // document.body.style.overflow = show ? "hidden" : "initial";
-
   menuSection.classList.toggle('on', show);
   show = !show;
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+  menuSection.classList.toggle('on', show);
+  show = !show;
+}
+
+navLink.forEach(link => {
+  link.addEventListener('click', linkAction);
 })
+
